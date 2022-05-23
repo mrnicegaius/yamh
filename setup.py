31108a94498adb52a34d2610837207351eef7259
mrnicegaius <78328014+mrnicegaius@users.noreply.github.com>
2022-05-23 14:00:31 +1000

    Updated README and updated setup.py to work with python build

--- setup.py
+++ setup.py
@@ -8,10 +8,13 @@ __version__ = "0.0.1"
 ext_modules = [
     Pybind11Extension("yamh",
             ["src/murmur3.cpp", "src/python.cpp"],
-            # Example: passing in the version to the compiled code
-            define_macros = [('VERSION_INFO', __version__)],
+            include_dirs=["src/"],
+            define_macros=[('VERSION_INFO', __version__)],
     ),
 ]
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="yamh",
@@ -20,16 +23,8 @@ setup(
     author_email="78328014+mrnicegaius@users.noreply.github.com",
     url="https://github.com/mrnicegaius/yamh",
     description="Yet another murmur3 hash library",
-    long_description="""
-Provides an interface to the murumurv3 hash library, but allow for streaming of
-data with a similar interface to the python hash libraries, ie:
-```python
-m = yamh.murmur3_32()
-m.update(b"Nobody inspects")
-m.update(b" the spammish repetition")
-m.digest()
-```
-""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     ext_modules=ext_modules,
     extras_require={"test": "pytest"},
     cmdclass={"build_ext": build_ext},
